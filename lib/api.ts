46e4c1eb16ac41b0458a40f908cdf963a3532b22
Rounak Harugire <rounakharugire3435@gmail.com>
2026-05-28 18:24:43 +0530

    Connected frontend to new backend

--- lib/api.ts
+++ lib/api.ts
@@ -3,13 +3,20 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://moneza-backend-dlhl.onrender.com/api/v1',
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
+    console.log("FULL ERROR:", error?.response);
+
     const message =
-      error.response?.data?.error || 'An unexpected error occurred';
+      error?.response?.data?.error ||
+      error?.response?.data?.message ||
+      'An unexpected error occurred';
 
     return Promise.reject(new Error(message));
   }
